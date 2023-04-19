b5f66cce94b31d2e6d0f4423d18c0a1090ff2356
Gigalskiy <gigalskiy@gmail.com>
2023-04-19 17:22:21 +0300

    write no reade

--- src/main/java/FileWRUD.java
+++ src/main/java/FileWRUD.java
@@ -7,26 +7,25 @@ public class FileWRUD {
 
     public static void main(String[] args) throws IOException {
 
-        File file = new File("file1.txt");
+        File file = new File("file");
 
-        int[][] arr = new int[36][3];
+        int[][] arr = new int[3][36];
         for (int z = 0; z < 3; z++) {
             for (int i = 0; i < 36; i++) {
                 if ((i + 1) % 3 == 0) {
-                    arr[i][z] = 1;
+                    arr[z][i] = 1;
                 } else {
-                    arr[i][z] = 0;
+                    arr[z][i] = 0;
                 }
             }
         }
 
         // записываем в файл ________________________________________________________
 
-       BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file,true));
+       BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file,false));
         for (int q=0; q<3; q++) {
             for (int j = 0; j < 36; j++) {
-                bufferedWriter.write(arr[j][q]);
-
+                bufferedWriter.write(arr[q][j]);
             }
             bufferedWriter.newLine();
         }
@@ -34,22 +33,35 @@ public class FileWRUD {
 
         // считываем из файла __________________________________________________________
 
-        int[][] arr1 = new int[36][3];
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        int[][] arr1 = new int[3][36];
+        FileInputStream fileInputStream = new FileInputStream(file);
+
         for (int q=0; q<3; q++) {
             for (int j = 0; j < 36; j++) {
-                arr1[j][q] = Integer.parseInt(bufferedReader.read());
-                System.out.println(arr1);
+                arr1[q][j] = (Integer)fileInputStream.read();
+
             }
-
+            fileInputStream.read();
         }
-        bufferedReader.close();
+        fileInputStream.close();
+
+        for (int q=0; q<3; q++) {
+            for (int j = 0; j < 36; j++) {
+                System.out.print(arr1[q][j]);
+            }
+            System.out.println();
+        }
+//        for (int q=0; q<3; q++) {
+//            for (int j = 0; j < 36; j++) {
+//                System.out.print(arr[q][j]);
+//            }
+//            System.out.println();
+//        }
 
 
 
 
-
-        System.out.println();
+//        System.out.println();
 
 
     }
